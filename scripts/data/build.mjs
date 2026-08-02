b02c02112a42029fe58bf4e727601bc57130f365
Kevin Weaver <kevinweaver2@gmail.com>
2026-08-02 00:15:27 -0700

    Forward pipeline bridge

--- scripts/data/build.mjs
+++ scripts/data/build.mjs
@@ -1,10 +1,4 @@
 const args = process.argv.slice(2)
+const { main } = await import('../pipeline/encode.ts')
 
-if (args.includes('--help')) {
-  console.log(
-    'Usage: data:build --input <encode-input.json> [--out <dir>] [--state <path>] [--generated-at <rfc3339>] [--dry-run]'
-  )
-} else {
-  const { main } = await import('../pipeline/encode.ts')
-  process.exitCode = await main(args)
-}
+process.exitCode = await main(args)
